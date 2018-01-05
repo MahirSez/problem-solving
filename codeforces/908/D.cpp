a7d63844a4ead6ce35c6f9463b7509134f679cd0
Mahir Shahriyar <mahirsezan@gmail.com>
2018-01-05 16:36:00 +0600

    Add solution for problem `D - New Year and Arbitrary Arrangement` Link: https://codeforces.com/contest/908/problem/D Tags: dp, math, probabilities, *2200 Ref: https://codeforces.com/contest/908/submission/33919849

--- codeforces/908/D.cpp
+++ codeforces/908/D.cpp
@@ -9,52 +9,50 @@
 #define INF     1e9
 #define EPS		1e-8
 #define MAX		1000006
-#define MOD		1000000007
+#define MOD     1000000007
 using namespace std;
-
-ll k , pa , pb ,ans;
 ll dp[2000][2000];
+ll  pa ,pb , k , p;
+
 
 ll bigMod(ll a , ll b) {
 
-    ll p ;
-    if( b == 0) return 1;
-    p = ( bigMod(a , b/2) % MOD );
-    p = ( p * p) % MOD;
-    if( b & 1 ) p = ( p * a) % MOD;
+    ll p;
+    if( b==0) return 1;
+    p = bigMod(a , b/2);
+    p = (p * p) % MOD;
+    if( b % 2) p = (p * a) % MOD;
     return p;
 }
 
-ll modInverse( ll x ) {
-    return bigMod(x, MOD-2);
-}
+ll modInverse(ll a) {return bigMod(a , MOD-2);}
 
-ll solve(ll a , ll ab) {
+
+ll solve(ll a, ll ab) {
 
     if( ab >=k ) return ab;
-
     if( a >= k ) {
-
-        ll tmp = ( (pb * (ab + a) ) % MOD + pa) % MOD;
-        tmp = ( tmp * modInverse(pb) ) % MOD;
-        return tmp;
+        ll ans = ( (pb * (ab +a) ) % MOD  + pa) % MOD;
+        ans = ( ans * modInverse(pb) ) % MOD;
+        return ans;
     }
 
     ll &ret = dp[a][ab];
-    if( ret != -1) return ret;
 
-    ll  cnt1 = ( solve( a+1 , ab) * pa ) % MOD;
-    cnt1 = (cnt1 * modInverse(pa + pb) ) % MOD;
-    ll cnt2 = (solve (a ,ab +a ) * pb) % MOD;
-    cnt2 = ( cnt2 * modInverse(pa + pb) ) % MOD;
-    return ret = ( cnt1 + cnt2 ) % MOD;
+    if( ret != -1 ) return ret;
+
+    ll cnt1 = ( solve( a+1 , ab) * pa ) % MOD;
+    cnt1 = (cnt1 * modInverse(p ) ) % MOD;
+    ll cnt2 = ( solve(a , ab+a ) * pb ) % MOD;
+    cnt2 = (cnt2 * modInverse(p ) ) % MOD;
+    return ret = (cnt1 + cnt2) % MOD;
 }
-
 
 int main()
 {
     cin>>k>>pa>>pb;
-    memset( dp , -1, sizeof(dp) ) ;
-    cout<<solve(1 , 0)<<endl;
+    p = pa + pb;
+    memset(dp , -1, sizeof(dp) ) ;
+    cout<<solve(1,0)<<endl;
     return 0;
 }
