100991e2652b3e4736916457f1c57054c5fd9796
Mahir Shahriyar <mahirsezan@gmail.com>
2018-11-14 06:47:00 +0600

    Add solution for problem `C - Watto and Mechanism` Link: https://codeforces.com/contest/514/problem/C Tags: binary search, data structures, hashing, string suffix structures, strings, *2000 Ref: https://codeforces.com/contest/514/submission/45694304

--- codeforces/514/C.cpp
+++ codeforces/514/C.cpp
@@ -12,56 +12,44 @@
 #define MOD     	1000000007
 #define fastRead 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
-int n , m ;
+int trie[MAX][3] , n , m ,root , sz;
+int en[MAX][3];
 string tgt;
 
-struct node {
-    int endMark;
-    node *next[3];
-    node() {
-        memset(next , 0 , sizeof next);
-        endMark = 0;
-    }
-} *root = new node();
 
-void Insert(int id , node* v) {
 
-    if(id == tgt.size() ) {
-        v->endMark  = 1;
-        return;
-    }
-    int col = tgt[id] - 'a';
+void Insert(string str) {
+    int len = str.size() , v = root ;
+    for(int i =0 ; i < len ; i++ ) {
 
-    if(v->next[col] == 0 ) {
-        v->next[col] = new node();
+        int id = str[i] - 'a';
+
+        if( trie[v][id] == 0 )  trie[v][id] = ++sz;
+        if( i == len -1 ) en[v][id]  =1 ;
+        v = trie[v][id];
     }
-    Insert(id+1 , v->next[col]);
 }
 
-bool Search(int id , int taken , node* v) {
 
-    if( taken > 1) return false;
-    if(id == tgt.size() ) {
-        return (taken == 1 && v->endMark ==1);
+bool Search(int id , int taken ,int rt, bool done) {
+
+    if( taken > 1 ) return false;
+    if( id == tgt.size() ) {
+        return ( taken == 1 && done );
     }
-
-    int col = tgt[id] - 'a';
-
     bool ret = false;
-    for(int i = 0; i < 3 ; i++ ) {
+    int col = tgt[id] - 'a';
+    if( trie[rt][col] ) {
+        ret = Search(id+1 , taken , trie[rt][col] , en[rt][col]);
+    }
+    for(int i =0 ; i < 3 ; i++ ) {
 
-        if( v->next[i] == NULL) continue;
-        else if( col == i) ret |= Search(id+1 , taken , v->next[i]);
-        else ret |= Search(id+1 , taken+1 , v->next[i]);
+        if( i == col ) continue;
+        if( trie[rt][i] == 0 ) continue;
+        ret |= Search(id+1 , taken+1 , trie[rt][i] , en[rt][i]);
     }
     return ret;
-}
 
-void del(node* v) {
-    for(int i =0 ; i < 3; i++ ) {
-        if( v->next[i] ) del(v->next[i]);
-    }
-    delete v;
 }
 
 int main()
@@ -69,17 +57,17 @@ int main()
     fastRead;
     cin>>n>>m;
     for(int i =0  ; i < n ; i++ ) {
-        cin>>tgt;
-        Insert( 0 , root);
+        string str;
+        cin>>str;
+        Insert(str);
     }
 
     for(int i =0 ; i < m ; i++) {
         cin>>tgt;
-        if( Search(0 , 0 , root) ) {
+        if( Search(0 , 0 , 0 , 0) ) {
             cout<<"YES\n";
         }
         else cout<<"NO\n";
     }
-    del(root);
     return 0;
 }
