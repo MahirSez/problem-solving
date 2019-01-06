41846c360c4462c8cfe36fc94715b957083192be
Mahir Shahriyar <mahirsezan@gmail.com>
2019-01-06 17:05:00 +0600

    Add solution for problem `A - Frog 1` Link: https://atcoder.jp/contests/dp/tasks/dp_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/3936302

--- atcoder/dp/A.cpp
+++ atcoder/dp/A.cpp
@@ -1,23 +1,39 @@
-#include<bits/stdc++.h>
-#define MAX 1000006
-#define INF 2e9
+#include <bits/stdc++.h>
+#define ll      	long long int
+#define PI      	acos(-1)
+#define read    	freopen("in.txt","r",stdin)
+#define uu      	first
+#define vv      	second
+#define write   	freopen("out.txt","w",stdout)
+#define pii     	pair<int,int>
+#define pll         pair<ll,ll>
+#define INF     	1e9
+#define EPS     	1e-8
+#define MAX     	1000006
+#define MOD     	1000000007
+#define fastRead 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
-int ara[MAX] , dp[MAX];
+int n ;
+ll ara[MAX] ,dp[MAX];
+
+ll solve(int id) {
+
+    if( id ==n-1 ) return 0;
+    ll &ret = dp[id];
+    if( ret != -1) return ret;
+
+    ret = abs(ara[id+1] - ara[id]) + solve(id+1);
+
+    if( id + 2 <n ) ret = min(ret , abs(ara[id+2] - ara[id]) + solve(id+2));
+    return ret;
+}
 
 int main()
 {
-    int n;
+    fastRead;
     cin>>n;
-    for(int i =0 ; i < n ; i++ ) cin>>ara[i];
-
-	for(int i =1 ; i < n ; i++ ) {
-
-        int val = INF;
-        if( i-1 >=0 ) val = min(val ,abs(ara[i] - ara[i-1]) + dp[i-1]);
-        if( i-2 >=0 ) val = min(val ,abs(ara[i] - ara[i-2]) +dp[i-2]);
-        dp[i] = val;
-	}
-	cout<<dp[n-1]<<endl;
-	return 0;
-
+    for(int i = 0 ; i < n ; i++ ) cin>>ara[i];
+    memset(dp , - 1, sizeof dp);
+    cout<<solve(0)<<endl;
+    return 0;
 }
