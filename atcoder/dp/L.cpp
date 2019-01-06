c297aed776fee6a9a799f149a7538c2b4f02d4c2
Mahir Shahriyar <mahirsezan@gmail.com>
2019-01-06 18:02:00 +0600

    Add solution for problem `L - Deque` Link: https://atcoder.jp/contests/dp/tasks/dp_l Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/3941133

--- atcoder/dp/L.cpp
+++ atcoder/dp/L.cpp
@@ -1,38 +1,43 @@
 #include <bits/stdc++.h>
-#define ll          long long int
-#define uu          first
-#define vv          second
-#define pii         pair<int,int>
+#define ll      	long long int
+#define PI      	acos(-1)
+#define read    	freopen("in.txt","r",stdin)
+#define uu      	first
+#define vv      	second
+#define write   	freopen("out.txt","w",stdout)
+#define pii     	pair<int,int>
 #define pll         pair<ll,ll>
-#define INF         1e9
-#define fastRead    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define INF     	1e9
+#define EPS     	1e-8
+#define MAX     	1000006
+#define MOD     	1000000007
+#define fastRead 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
+int n ;
+ll ara[3003] ,dp[3003][3003][2];
 
-const int MAX = 1e6 + 6;
+ll solve(int l , int r , int turn) {
+
+    if(l  > r) return 0;
+
+    ll &ret= dp[l][r][turn];
+    if( ret != -1) return ret;
+
+
+    if( turn == 1 )
+        ret = min( solve(l+ 1 , r , 0) - ara[l]  , solve(l , r-1 , 0) - ara[r]);
+    else
+        ret = max( solve(l+ 1 , r , 1) + ara[l]  , solve(l , r-1 , 1) + ara[r]);
+    return ret;
+}
+
 int main()
 {
     fastRead;
-    int n;
     cin>>n;
-    vector<ll>vec(n);
-    vector< vector<ll> > dp( n+1, vector<ll>(n+1) );
-    for(ll& x : vec) cin>>x;
-    
-    for(int i =0 ; i <n ; i++) {
-        if(n&1) dp[1][i] = vec[i];
-        else dp[1][i] = -vec[i];
-    }
-    
-    for(int i = 2; i <= n ; i++ ) {
-        for(int j =0 ; j + i - 1 < n ; j++ ) {
-            if( ((n%2) ^ (i%2))  == 0 )
-                dp[i][j] = max(dp[i-1][j] + vec[i + j - 1], dp[i-1][j+1] + vec[j]);
-            else
-                dp[i][j] = min(dp[i-1][j] - vec[i + j - 1], dp[i-1][j+1] - vec[j]);
-            
-        }
-    }
-    
-    cout<<dp[n][0]<<'\n';
+    for(int i =0 ; i < n ; i++ ) cin>>ara[i];
+    memset(dp , - 1, sizeof dp);
+    cout<<solve(0 , n-1 , 0)<<endl;
     return 0;
 }
+
