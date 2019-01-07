e963592891496829cd30dc2a4128173c77e89818
Mahir Shahriyar <mahirsezan@gmail.com>
2019-01-07 13:14:00 +0600

    Add solution for problem `E - Knapsack 2` Link: https://atcoder.jp/contests/dp/tasks/dp_e Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/3954352

--- atcoder/dp/E.cpp
+++ atcoder/dp/E.cpp
@@ -1,39 +1,56 @@
-#include<bits/stdc++.h>
-#define MAX 100005
-#define INF 2e9
-#define ll long long int
-#define fastRead ios::sync_with_stdio(0);cin.tie(0);
-
+#include <bits/stdc++.h>
+#define ll      	long long int
+#define PI      	acos(-1)
+#define read    	freopen("in.txt","r",stdin)
+#define uu      	first
+#define vv      	second
+#define write   	freopen("out.txt","w",stdout)
+#define pii     	pair<int,int>
+#define pll         pair<ll,ll>
+#define INF     	1e18
+#define EPS     	1e-8
+#define MAX     	1000006
+#define MOD     	1000000007
+#define fastRead 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
-int n;
-ll v[200] , w[200] ,dp[200][MAX] , W;
+ll n , w;
+ll wt[200];
+int val[200] ;
+ll dp[200][100005];
+
+ll solve(int id , int tgt) {
+
+    if( tgt < 0) return INF;
+    if( id == n ) {
+        if( tgt == 0 ) return  0;
+        return INF;
+    }
+    ll &ret = dp[id][tgt];
+    if( ret != -1 ) return ret;
+
+    ret =solve(id+1 , tgt);
+
+    ret = min( ret , wt[id] + solve(id+1 , tgt-val[id] ) );
+    return ret;
+}
 
 int main()
 {
-	fastRead;
-	cin>>n>>W;
-
-	ll tot = 0;
-	for(int i =1 ; i<= n ; i++ ) cin>>w[i]>>v[i] , tot +=v[i];
-
-
-	ll ans = 0;
-    for(int i = 1 ; i<= tot ; i++ ) {
-
-        dp[0][i] = INF;
-
-        for(int j = 1 ; j <= n ; j++ )  {
-
-            if( v[j] <= i ) {
-				dp[j][i] = min(dp[j-1][i] , dp[j-1][i-v[j]]  + w[j]);
-            }
-            else dp[j][i] = dp[j-1][i];
-        }
-        if(	dp[n][i] <= W) ans = max(1LL*i  , ans);
+    fastRead;
+    cin>>n>>w;
+    int tot = 0;
+    for(int i =0 ; i < n ; i++ ) {
+        cin>>wt[i]>>val[i];
+        tot += val[i];
     }
 
+    memset(dp , -1 , sizeof dp);
+    int ans = 0;
+    for(int i = 1 ; i <=  tot; i++ ) {
+
+        ll wt = solve(0 , i);
+        if( wt <= w) ans = i;
+    }
     cout<<ans<<endl;
     return 0;
-
 }
-
