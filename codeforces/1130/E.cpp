7903e50bc109c3ca7e46d7ead580d7fbefc9825f
Mahir Shahriyar <mahirsezan@gmail.com>
2019-02-25 03:15:00 +0600

    Add solution for problem `E - Wrong Answer` Link: https://codeforces.com/contest/1130/problem/E Tags: brute force, constructive algorithms, math, *2000 Ref: https://codeforces.com/contest/1130/submission/50468078

---
+++ codeforces/1130/E.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll      	long long int
+#define PI      	acos(-1)
+#define read    	freopen("in.txt","r",stdin)
+#define uu      	first
+#define vv      	second
+#define write   	freopen("out.txt","w",stdout)
+#define pii     	pair<int,int>
+#define INF     	1e9
+#define EPS     	1e-8
+#define MAX     	1000006
+#define MOD     	1000000007
+#define fastRead 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+int k , n;
+int main()
+{
+    cin>>k;
+    n = 2000;
+
+    cout<<n<<endl;
+    cout<<-1<<" ";
+    
+    int tgt = k + n , mx = 1000000;
+
+    for(int i =1 ; i< n ; i++) {
+
+        if(tgt > mx) {
+            cout<<mx<<" ";
+            tgt -= mx;
+        }
+        else if( tgt ==0) {
+            cout<<0<<" ";
+        }
+        else {
+            cout<<tgt<<" ";
+            tgt = 0;
+        }
+    }
+    cout<<endl;
+    return 0;
+}
