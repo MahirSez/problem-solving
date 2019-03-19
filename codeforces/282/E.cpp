c2b2e3bd798124446d9b58cd82819f8e02546361
Mahir Shahriyar <mahirsezan@gmail.com>
2019-03-20 00:29:00 +0600

    Add solution for problem `E - Sausage Maximization` Link: https://codeforces.com/contest/282/problem/E Tags: bitmasks, data structures, trees, *2200 Ref: https://codeforces.com/contest/282/submission/51541509

---
+++ codeforces/282/E.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define MAX 100005
+using namespace std;
+ll n , ara[MAX];
+
+struct node {
+    node* next[2];
+    node() {
+    	memset(next , 0 , sizeof next);
+    }
+}*root ;
+
+
+
+void Insert(ll num) {
+
+	node *rt = root;
+	for(int i =50 ; i>=0 ; i--) {
+
+        int id = bool(num&(1LL<<i) );
+        if(rt->next[id] ==0 ) rt->next[id] = new node();
+
+        rt = rt->next[id];
+	}
+}
+
+ll totXor = 0;
+
+ll query(ll num) {
+
+	ll ans =0 ;
+
+	node *rt = root;
+
+    for(int i =50 ; i>=0; i--) {
+
+        int id = bool(num & (1LL<<i) );
+
+        if( rt->next[id^1] ) {
+			ans = (ans<<1|1);
+			rt = rt->next[id^1];
+        }
+        else {
+			ans= ans<<1;
+			rt = rt ->next[id];
+        }
+    }
+    return ans;
+}
+
+
+void del(node* rt) {
+    if( rt->next[0]) del(rt->next[0]);
+    if( rt->next[1]) del(rt->next[1]);
+    delete rt;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	root = new node();
+	cin>>n;
+	for(int i =0 ; i < n ; i++ ) cin>>ara[i] ,totXor ^= ara[i];
+
+    ll ans = totXor;
+
+    ll tmpXor = 0;
+
+    Insert(0);
+
+    for(int i =0 ; i <n ; i++  ) {
+
+
+        tmpXor ^= ara[i];
+
+        Insert(tmpXor);
+
+        ans = max(ans , query(totXor ^ tmpXor) ) ;
+//		cout<<tmpXor<<" "<<(totXor^tmpXor)<<" "<<query(totXor^tmpXor)<<endl;
+    }
+
+    cout<<ans<<endl;
+    del(root);
+    return 0;
+
+}
