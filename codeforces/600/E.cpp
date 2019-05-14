a6c180d97df3d2fdba6ad17cd291d1e0accd2043
Mahir Shahriyar <mahirsezan@gmail.com>
2019-05-14 08:52:00 +0600

    Add solution for problem `E - Lomsat gelral` Link: https://codeforces.com/contest/600/problem/E Tags: data structures, dfs and similar, dsu, trees, *2300 Ref: https://codeforces.com/contest/600/submission/54080787

--- codeforces/600/E.cpp
+++ codeforces/600/E.cpp
@@ -13,77 +13,63 @@
 #define MOD     	1000000007
 #define fastRead 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
-int n , col[MAX] , sz[MAX], mxCnt[MAX] , st[MAX] , fn[MAX] , _time , id[MAX] , cnt[MAX];
+int n , col[MAX] , sz[MAX], mxCnt[MAX] ;
 ll ans[MAX];
+map<int,int>*mp[MAX];
 vector<int>edg[MAX];
 
 void dfs1(int node , int par) {
 
 	sz[node] = 1;
-    st[node] = ++_time;
-    id[_time] = node;
-
 	for(auto x : edg[node]) {
 		if( x== par) continue;
         dfs1(x , node);
         sz[node] += sz[x];
 	}
-	fn[node] = ++_time;
 }
 
-void dfs2(int node , int par, int keep) {
+void dfs2(int node , int par) {
 
     int bigChild = -1;
 
-    for(auto x : edg[node]) {
-
-        if( x == par) continue;
-        if( bigChild == -1 || sz[x] > sz[bigChild] ) bigChild = x;
+    for(auto x: edg[node]) {
+        if( x== par) continue;
+        dfs2(x , node);
+        if( bigChild == -1 || sz[x] > sz[bigChild]) bigChild = x;
     }
 
-
-    for(auto x : edg[node]) {
-        if( x == par || x == bigChild) continue;
-        dfs2(x , node , 0);
-
-    }
-
-    if(bigChild != -1 ) {
-
-        dfs2(bigChild , node , 1);
-        mxCnt[node] = mxCnt[bigChild];
+    if( bigChild == -1 ) mp[node] = new map<int,int>;
+    else {
+		mp[node] = mp[bigChild];
         ans[node] = ans[bigChild];
+        mxCnt[node] = mxCnt[bigChild];
     }
-    cnt[col[node]]++;
 
-    if( cnt[col[node]] > mxCnt[node]) mxCnt[node] = cnt[col[node]] , ans[node] = col[node];
-    else if( cnt[col[node]] == mxCnt[node] ) ans[node] += col[node];
+    (*mp[node])[ col[node] ]++;
 
+    int cnt = (*mp[node])[col[node]];
 
-    for(auto x : edg[node]) {
-		if( x == par || x == bigChild ) continue;
+    if(cnt > mxCnt[node]) {
+		mxCnt[node] = cnt;
+        ans[node] = col[node];
+    }
+    else if( cnt == mxCnt[node]) {
+		ans[node] += col[node];
+    }
+    for(auto x: edg[node]) {
 
-        for(int i = st[x] ;  i<= fn[x] ; i++ ) {
+        if( x== par || x == bigChild) continue;
 
-            if( id[i] ==0 ) continue;
-            int xx = id[i];
+        for(auto xx : *mp[x]) {
 
-            cnt[col[xx]]++;
+            (*mp[node])[xx.uu] += xx.vv;
+            int cnt = ((*mp[node])[xx.uu]);
 
-            if(cnt[col[xx]] > mxCnt[node]) mxCnt[node] = cnt[col[xx]] , ans[node] = col[xx];
-            else if( cnt[col[xx]] == mxCnt[node] ) ans[node] += col[xx];
+            if( cnt > mxCnt[node])  mxCnt[node] = cnt , ans[node] = xx.uu;
+            else if( cnt == mxCnt[node]) ans[node] += xx.uu;
         }
+
     }
-
-    if( keep == 0 ) {
-
-        for(int i = st[node] ; i <= fn[node] ; i++ ) {
-            if( id[i] ==0 ) continue;
-            int xx = id[i];
-            cnt[col[xx]]--;
-        }
-    }
-
 }
 
 int main()
@@ -98,7 +84,7 @@ int main()
 		edg[v].push_back(u);
 	}
     dfs1(1 , -1);
-    dfs2(1 , -1, 1);
+    dfs2(1 , -1);
 
     for(int i =1 ; i<= n ; i++ ) {
 		cout<<ans[i]<<" ";
