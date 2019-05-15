387a401f2059fea10ffab7e6a7ba15a72966ddd6
Mahir Shahriyar <mahirsezan@gmail.com>
2019-05-15 09:14:00 +0600

    Add solution for problem `E - Blood Cousins` Link: https://codeforces.com/contest/208/problem/E Tags: binary search, data structures, dfs and similar, trees, *2100 Ref: https://codeforces.com/contest/208/submission/54154559

--- codeforces/208/E.cpp
+++ codeforces/208/E.cpp
@@ -9,129 +9,91 @@
 #define pll			pair<ll,ll>
 #define INF     	1e9
 #define EPS     	1e-8
-#define MAX     	100005
+#define MAX     	1000006
 #define MOD     	1000000007
 #define fastRead 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
+int n ,_time , st[MAX] , en[MAX] , par[MAX] , table[MAX][20] , level[MAX];
+vector<int>lvl[MAX] , edg[MAX] ;
 
-int n ,table[20][MAX] , sz[MAX], lvl[MAX] , rec[MAX] , ans[MAX];
-vector<int>edg[MAX] , *vec[MAX] ;
-vector<pii> str[MAX];
+void dfs(int node , int lev) {
 
-void dfs1(int node ,int lev){
-    
-    sz[node] =1;
-    lvl[node] = lev;
+
+	level[node] = lev;
+    st[node] = ++_time;
+    lvl[lev].push_back(_time);
+
     for(auto x : edg[node]) {
-        dfs1(x , lev+1);
-        sz[node] += sz[x];
-    }
-}
 
+		table[x][0] = node;
+        dfs(x , lev+1);
+    }
+    en[node] = ++_time;
+}
 void build() {
-    
-    for(int i = 1 ; (1<<i) <=n ; i++ ) {
-        
-        for(int j = 1 ; j <=n ; j++ ) {
-            
-            if( table[i-1][j] != -1) table[i][j] = table[i-1] [ table[i-1][j] ];
+
+
+    for(int i = 1 ; (1<<i) <= n ; i++ ) {
+
+        for(int j = 1 ; j <= n ; j++ ) {
+
+            if( table[j][i-1] != -1) {
+                table[j][i] = table[ table[j][i-1] ][i-1];
+            }
         }
     }
 }
 
-int getKth(int node , int k) {
-    
-    for(int i = 20 ; i >= 0 ; i-- ) {
-        
-        if( k >= (1<<i) && table[i][node] != -1 ) {
-            node = table[i][node];
-            k-=(1<<i);
+int getPar(int node , int k) {
+
+	for(int i = 20 ; i >= 0 ; i-- ) {
+
+        if( (1<<i) <=k && table[node][i] != -1) {
+            node = table[node][i];
+            k -= (1<<i);
         }
-    }
-    if( k ) return 0;
+	}
+	if( k != 0) return -1;
     return node;
 }
-
-void dfs2(int node , int keep) {
-    
-    int bigChild = -1;
-    
-    for(auto x : edg[node]) {
-        if( bigChild == -1 || sz[x] > sz[bigChild]) bigChild = x;
-    }
-    
-    for(auto x : edg[node]) {
-        if( x == bigChild) continue;
-        dfs2(x , 0);
-    }
-    
-    if( bigChild == -1) vec[node] = new vector<int>();
-    else {
-        dfs2(bigChild , 1);
-        vec[node] = vec[bigChild];
-    }
-    vec[node]->push_back(node);
-    rec[lvl[node]]++;
-    
-    for(auto x : edg[node]) {
-        if( x == bigChild) continue;
-        
-        for(auto xx : *vec[x]) {
-            
-            vec[node]->push_back(xx);
-            rec[lvl[xx]]++;
-        }
-    }
-    
-    for(auto x : str[node]) {
-        
-        int h = x.uu;
-        int id = x.vv;
-        
-        if( node == 0 ) ans[id] = 0;
-        else {
-            ans[id] = rec[h] -1;
-        }
-    }
-    if( keep == 0) {
-        for(auto x : *vec[node]) {
-            rec[lvl[x]]--;
-        }
-    }
-}
-
 
 int main()
 {
     fastRead;
     cin>>n;
-    memset(table , -1  , sizeof table);
-    for(int i =1 ; i<= n ; i++ ) {
-        
-        int par;
-        cin>>par;
-        table[0][i] = par;
-        edg[par].push_back(i);
+    for(int i =1 ; i <=n ; i++ ) {
+		int p;
+		cin>>p;
+        edg[p].push_back(i);
     }
-    
-    dfs1(0, 1);
-    
-    build();
-    
+
+	memset(table , -1 , sizeof table);
+    dfs(0 , 0);
+
+	build();
+
+
     int m;
     cin>>m;
-    for(int i = 1 ; i <= m ; i++ ) {
-        
-        int v , k;
-        cin>>v>>k;
-        int pp = getKth(v , k);
-        str[pp].push_back({lvl[v] , i});
+    while( m-- ) {
+
+        int v , p;
+        cin>>v>>p;
+        int pp = getPar(v , p);
+        if(pp <=0 ) {
+			cout<<0<<" " ;
+			continue;
+        }
+
+        int h = level[v];
+
+        int it1 = lower_bound( lvl[h].begin() , lvl[h].end() , st[pp]) - lvl[h].begin();
+        int it2 = upper_bound( lvl[h].begin() , lvl[h].end() , en[pp]) - lvl[h].begin();
+
+		cout<<it2-it1-1<<" ";
     }
-    
-    dfs2(0 , 1);
-    for(int i =1 ; i<= m ; i++) {
-        cout<<ans[i]<<" ";
-    }
-    cout<<endl;
-    return 0;
+
+	cout<<endl;
+	return 0;
+
 }
