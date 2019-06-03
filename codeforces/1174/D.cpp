7970b1e6b10c09abd166b2bacdfebb68bb1eaa1c
Mahir Shahriyar <mahirsezan@gmail.com>
2019-06-03 22:03:00 +0600

    Add solution for problem `D - Ehab and the Expected XOR Problem` Link: https://codeforces.com/contest/1174/problem/D Tags: bitmasks, constructive algorithms, *1900 Ref: https://codeforces.com/contest/1174/submission/55049671

--- codeforces/1174/D.cpp
+++ codeforces/1174/D.cpp
@@ -9,31 +9,44 @@
 #define pll         pair<ll,ll>
 #define INF         1e9
 #define EPS         1e-8
-#define MAX         1000006
+#define MAX         100005  
 #define MOD         1000000007
 #define fastRead    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
-int n , x , pre[MAX] , taken[MAX];
+int n , x;
 vector<int>ans;
+
 int main()
 {
     cin>>n>>x;
-    taken[x] = 1;
+    int msb = -1;
+    for(int i =0 ; i < 31 ; i++ ) {
+        if( x & (1<<i) ) msb = i;
+    }
+    int lastBit=-1;
+    
+//    if( x ==1 ) ans.push_back(2) , lastBit = 1;
+//    else ans.push_back(1) , lastBit = 0;
     
     
-    for(int i = 1; i < (1<<n) ; i++ ) {
+    while(lastBit +1< n) {
+        
+        lastBit++;
+        if( lastBit == msb) {
             
-        if( taken[i] ) continue;
-        ans.push_back(i);
-        taken[x^i] =1;
+            continue;
+        }
+        ans.push_back((1<<lastBit) );
+        int nn = ans.size();
+        
+        for(int i =0 ; i < nn -1 ; i++ ) {
+            ans.push_back(ans[i]);
+        }
     }
-    int prev = 0;
-    
-    cout<<ans.size()<<endl;
-    for(int i =0 ; i < ans.size() ;i++) {
-        cout<<(ans[i]^prev)<<" ";
-        prev = ans[i];
+    cout<<ans.size()<<'\n';
+    for(auto x: ans) {
+        cout<<x<<" ";
     }
-    cout<<endl;
+    cout<<'\n';
     return 0;
 }
