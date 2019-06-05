034cfd2cc13b957e4ea36b24590bf6c3e54bdfa5
Mahir Shahriyar <mahirsezan@gmail.com>
2019-06-05 20:40:00 +0600

    Add solution for problem `A - From Hero to Zero` Link: https://codeforces.com/contest/1175/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1175/submission/55131776

---
+++ codeforces/1175/A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define PI          acos(-1)
+#define read        freopen("in.txt","r",stdin)
+#define uu          first
+#define vv          second
+#define write       freopen("out.txt","w",stdout)
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define INF         1e9
+#define EPS         1e-8
+#define MAX         1000006
+#define MOD         1000000007
+#define fastRead    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+int t;
+ll n ,k;
+
+int main()
+{
+    fastRead;
+    cin>>t;
+    while( t-- ) {
+        
+        cin>>n>>k;
+        
+        ll ans = 0;
+        while( n > 0 ) {
+            
+            if( n%k ==0 ) {
+                ans ++;
+                n = n / k;
+                continue;
+            }
+            
+            ll tmp = (n/k ) *k;
+            
+            ans += (n - tmp);
+            n = tmp;    
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
