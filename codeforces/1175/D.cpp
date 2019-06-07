de7a1ceeb6bb6265887115a41d86954a218e052d
Mahir Shahriyar <mahirsezan@gmail.com>
2019-06-07 10:56:00 +0600

    Add solution for problem `D - Array Splitting` Link: https://codeforces.com/contest/1175/problem/D Tags: greedy, sortings, *1900 Ref: https://codeforces.com/contest/1175/submission/55229948

---
+++ codeforces/1175/D.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define PI          acos(-1)
+#define read        freopen("in.txt","r",stdin)
+#define uu          first
+#define vv          second
+#define write       freopen("out.txt","w",stdout)
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define INF         1e9
+#define EPS         1e-8
+#define MAX         1000006
+#define MOD         1000000007
+#define fastRead    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+int n , k;
+ll ara[MAX] , sum[MAX];
+vector<ll>v;
+int main()
+{
+    fastRead;
+    cin>>n>>k;
+    for(int i = 1 ; i <= n ; i++ ) cin>>ara[i];
+    for(int i = 1 ; i <= n ; i++ ) {
+        sum[i] = sum[i-1] + ara[i];
+        if(i < n )v.push_back(sum[i]);
+    }
+    
+    ll ans = sum[n]*k;
+    sort(v.begin() , v.end());
+    for(int i =0 ; i < k -1 ; i++ ) {
+        ans -= v[i];
+    }
+    cout<<ans<<'\n';
+    return 0;
+    
+}
