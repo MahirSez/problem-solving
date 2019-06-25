cce83a6084a196c11abce34b120a10d60c0c3c6e
Mahir Shahriyar <mahirsezan@gmail.com>
2019-06-26 03:34:00 +0600

    Add solution for problem `B - Working out` Link: https://codeforces.com/contest/429/problem/B Tags: dp, *1600 Ref: https://codeforces.com/contest/429/submission/56054228

--- codeforces/429/B.cpp
+++ codeforces/429/B.cpp
@@ -6,51 +6,63 @@ int n , m ;
 ll ara[4000][4000] ;
 ll dp[4][2000][2000];
 
-int main()
-{
+
+ll solve(int id , int x , int y) {
+
+    if( x < 0 || y < 0 || x >= n || y >= m) return -1e18;
+
+    if( id ==0 && x == 0 && y ==0)  return ara[x][y];
+    if( id == 3 && x == n-1 && y == m-1) return ara[x][y];
+    
+    if( id == 1 && x== n-1 && y ==0  ) return ara[x][y];
+    if( id==2 && x==0 && y== m-1) return ara[x][y];
+
+
+    ll &ret = dp[id][x][y];
+    if( ret != -1) return ret ;
+
+    ret = 0;
+
+    if( id == 0 ) ret = max( solve(id , x-1 , y) , solve(id ,x, y-1 ) );
+    if( id == 3 ) ret = max( solve(id , x+1 , y) , solve(id ,x , y+1 ) );
+    
+    
+    if( id == 1 ) ret = max( solve(id , x+1 , y) , solve(id ,x , y-1 ) );
+    if( id == 2 ) ret = max( solve(id , x-1 , y) , solve(id ,x,  y+1 ) );
+    
+    ret += ara[x][y];
+    return ret ;
+}
+
+int main() {
+
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>n>>m;
 
-    for(int i = 1 ; i  <= n ; i++) {
+    for(int i = 0 ; i  < n ; i++) {
 
-        for(int j =1 ; j <= m ; j++ ) {
+        for(int j =0 ; j < m ; j++ ) {
 
             cin>>ara[i][j];
         }
     }
-    
-    for(int i = 1; i <= n ; i++ ) {
-        for(int j =1 ; j <= m ; j++ ) {
-            dp[0][i][j] = ara[i][j] + max(dp[0][i-1][j] , dp[0][i][j-1]);
-        }
-    }
-    
-    for(int i = n ; i>=1 ; i--) {
-        for(int j = m ; j>=1 ; j-- ) {
-            dp[1][i][j] = ara[i][j] + max(dp[1][i+1][j] , dp[1][i][j+1]);
-        }
-    }
-    
-    for(int i = 1 ; i <= n ; i++ ) {
-        for(int j = m ; j>=1 ; j--) {
-            dp[2][i][j] = ara[i][j] + max(dp[2][i-1][j] , dp[2][i][j+1]);
-        }
-    }
-    
-    for(int i = n ; i >=1 ; i-- ) {
-        for(int j = 1 ; j <= m ; j++ ) {
-            dp[3][i][j] = ara[i][j] + max(dp[3][i+1][j] , dp[3][i][j-1]);
-        }
-    }
+
+    memset(dp , -1 , sizeof dp);
+
     ll ans = 0;
-    
-    for(int i = 2 ; i <= n -1; i++ ) {
-        for(int j = 2 ; j <= m-1 ; j++ ) {
-            ans = max(ans , dp[0][i-1][j] + dp[1][i+1][j] + dp[2][i][j+1] + dp[3][i][j-1]);
-            ans = max(ans , dp[0][i][j-1] + dp[1][i][j+1] + dp[2][i-1][j] + dp[3][i+1][j]);
+
+    for(int i = 0 ; i  < n ; i++) {
+
+        for(int j =0 ; j < m ; j++ ) {
+
+            ans = max(ans , solve(0,i-1,j) + solve(3,i+1,j) + solve(1,i,j-1) + solve(2,i,j+1)  );
+            ans = max(ans , solve(0 ,i , j-1 ) + solve(3 , i , j+1 ) + solve( 1 , i+1 , j) + solve(2 , i-1 , j ) ) ;
         }
     }
+
     cout<<ans<<'\n';
     return 0;
+
+
 }
