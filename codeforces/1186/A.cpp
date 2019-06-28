7f8e259a66e8afd201decaba56b7687b0d76ab29
Mahir Shahriyar <mahirsezan@gmail.com>
2019-06-28 14:21:00 +0600

    Add solution for problem `A - Vus the Cossack and a Contest` Link: https://codeforces.com/contest/1186/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1186/submission/56193428

---
+++ codeforces/1186/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define PI          acos(-1)
+#define read        freopen("in.txt","r",stdin)
+#define uu          first
+#define vv          second
+#define write       freopen("out.txt","w",stdout)
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define INF         1e9
+#define EPS         1e-8
+#define MAX         1000006
+#define MOD         1000000007
+#define fastRead    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+int n , m , k;
+int main()
+{
+    cin>>n>>m>>k;
+    if( n <=m && n <= k ) {
+        cout<<"Yes\n";
+    }
+    else cout<<"No\n";
+    return 0;
+}
