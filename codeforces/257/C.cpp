bff8f70c8c6520076952ab1c48c24b6e911967f8
Mahir Shahriyar <mahirsezan@gmail.com>
2019-08-14 16:59:00 +0600

    Add solution for problem `C - View Angle` Link: https://codeforces.com/contest/257/problem/C Tags: brute force, geometry, math, *1800 Ref: https://codeforces.com/contest/257/submission/58811441

--- codeforces/257/C.cpp
+++ codeforces/257/C.cpp
@@ -13,6 +13,37 @@ const double PI  = acos(-1);
 
 vector<double>vec;
 
+
+double getAngle(double x , double y) {
+
+
+    
+    if( x == 0) {
+        
+        if( y > 0 ) return 90;
+        return 270;
+    }
+    if(y ==0 ) {
+        
+        if( x > 0  ) return 0;
+        return 180;
+    }
+    
+    double angle = 0;
+    if( x > 0 ) {
+        
+        if( y > 0) angle  =  atan(y/x) ;
+        else angle  =  2*PI - atan( (-1.0*y) / x);
+    }
+    else {
+        if( y > 0 ) angle = PI - atan(y / (-1.0*x) );
+        else angle = PI + atan(  (-1.0*y) / (-1.0*x ) );
+    }
+    
+    return (angle * 180.0)/PI;
+    
+}
+
 int main()
 {
     fastRead;
@@ -22,16 +53,26 @@ int main()
         
         double x , y;
         cin>>x>>y;
-        vec.push_back(atan2(y,x)*180.0/PI);
+        
+        vec.push_back(getAngle(x,y));
     }
-
+    if( n ==1 ) {
+        cout<<0<<endl;
+        return 0;
+    }
+    
     sort(vec.begin() , vec.end());
     
-    double gap = 360.0 - vec[n-1] + vec[0];
+    double gap = 0;
     for(int i =0 ; i < n-1 ; i++ ) {
+        
         gap = max(gap , vec[i+1] - vec[i]);
     }
-    printf("%0.9f\n",360.0 - gap);
+    
+    gap = max(gap , 360.0 - vec[n-1] + vec[0]);
+    
+    double ans = 360.0 - gap;
+    printf("%0.9f\n",ans);
     return 0;
     
 }
