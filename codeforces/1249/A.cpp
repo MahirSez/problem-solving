cd7b184fd9c9f17a1511123d72d13a12f5b4c243
Mahir Shahriyar <mahirsezan@gmail.com>
2019-10-22 20:37:00 +0600

    Add solution for problem `A - Yet Another Dividing into Teams` Link: https://codeforces.com/contest/1249/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1249/submission/63132500

--- codeforces/1249/A.cpp
+++ codeforces/1249/A.cpp
@@ -1,29 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define ll          long long int
+#define uu          first
+#define vv          second
+#define pii         pair<int,int>
+#define INF         1e9
+#define MOD         1000000007
+#define fastRead    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
+
+const int MAX = 1e6 + 6;
+int ara[200];
 int main()
-{   
+{
     int t;
-    scanf("%d",&t);
+    cin>>t;
     while(t--) {
-        int n;
-        vector<int>vec;
-        scanf("%d",&n);
         
-        for(int i =0 ; i < n ; i ++ ) {
-            int tmp;
-            scanf("%d",&tmp);
-            vec.push_back(tmp);
-        }
-        sort(vec.begin() , vec.end() );
+        int n;
+        cin>>n;
+        for(int i =0 ; i < n ; i++ ) cin>>ara[i];
+        sort(ara, ara  + n);
         int ans = 1;
         
         for(int i =1 ; i < n ; i++ ) {
-            if(vec[i] == vec[i-1] + 1) {
+            if(ara[i] == ara[i-1] + 1) {
                 ans++;
                 break;
             }
         }
-        printf("%d\n",ans);
+        cout<<ans<<'\n';
     }
     return 0;
 }
