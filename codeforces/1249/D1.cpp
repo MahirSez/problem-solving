76e737e547a5e74f68bfabe16cd9e38f435a5d3e
Mahir Shahriyar <mahirsezan@gmail.com>
2019-10-22 23:09:00 +0600

    Add solution for problem `D1 - Too Many Segments (easy version)` Link: https://codeforces.com/contest/1249/problem/D1 Tags: greedy, *1800 Ref: https://codeforces.com/contest/1249/submission/63190662

--- codeforces/1249/D1.cpp
+++ codeforces/1249/D1.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 #define ll      	long long int
 #define PI      	acos(-1)
@@ -9,22 +8,22 @@
 #define pii     	pair<int,int>
 #define INF     	1e9
 #define EPS     	1e-8
-
+ 
 #define MOD     	1000000007
 #define fastRead 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
-
+ 
 const int MAX = 2e5 + 5;
 int n ,k;
 int  tree[4*MAX] ,lazy[4*MAX];
-
+ 
 struct line {
     
     int l , r , id;
 };
-
+ 
 line ara[MAX];
-
+ 
 void push(int node ,int l ,int r) {
     if( lazy[node]) {
         tree[node] += lazy[node];
@@ -35,13 +34,13 @@ void push(int node ,int l ,int r) {
         lazy[node] = 0;
     }
 }
-
-
+ 
+ 
 void print() {
     for(int i =1; i<=4*n ; i++ ) cout<<tree[i]<<" ";
     cout<<endl;
 }
-
+ 
 void update(int node ,int l, int r, int frm, int to ,int val) {
     push(node, l , r);
     if( l > r || l > to || r < frm ) return ;
@@ -55,7 +54,7 @@ void update(int node ,int l, int r, int frm, int to ,int val) {
     update(node<<1|1 , mid+1 , r , frm ,to , val);
     tree[node] = max( tree[node<<1] , tree[node<<1|1] );
 }
-
+ 
 int query(int node, int l , int r ,int frm ,int to ) {
     push(node, l , r);
     if( l > r || l > to || r < frm ) return 0;
@@ -67,15 +66,15 @@ int query(int node, int l , int r ,int frm ,int to ) {
     int q2 = query(node<<1|1 , mid+1 , r , frm , to);
     return max(q1,q2);
 }
-
+ 
 bool comp(line p1 , line p2)  {
-    return (p1.r < p2.r);
+    return (p1.r < p2.r );
 }
-
-
+ 
+ 
 vector<int>tmp;
-bool ok (int tgt) {
-
+void solve() {
+ 
 //    cout<<tgt<<'\n';
     tmp.clear();
     memset(tree , 0 , sizeof tree);
@@ -93,10 +92,9 @@ bool ok (int tgt) {
         else update(1 , 1 , 2e5 , frm , to , 1);
     }
     
-    return ( tmp.size() <= tgt);
 }
-
-
+ 
+ 
 int main()
 {
     fastRead;
@@ -110,22 +108,25 @@ int main()
     
     
     int lo = 0 , hi = n;
-
+ 
     
     vector<int>ans;
     
     
-    while(lo<= hi) {
-        
-       int mid = (lo+hi) /2;
-       
-        if(ok(mid)) {
-            ans = tmp;
-            hi = mid - 1;
-        }
-        else lo = mid+1;
-    }
+//    while(lo<= hi) {
+//        
+//       int mid = (lo+hi) /2;
+//       
+//        if(ok(mid)) {
+//            ans = tmp;
+//            hi = mid - 1;
+//        }
+//        else lo = mid+1;
+//    }
     
+    
+    solve();
+    ans = tmp;
     cout<<ans.size()<<'\n';
     for(auto x : ans) cout<<x<<" ";
     cout<<'\n';
