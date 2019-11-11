c9dbcde3879b4da6582309a56a4b6618e8bc1533
Mahir Shahriyar <mahirsezan@gmail.com>
2019-11-11 22:39:00 +0600

    Add solution for problem `B - Alice and the List of Presents` Link: https://codeforces.com/contest/1236/problem/B Tags: combinatorics, math, *1500 Ref: https://codeforces.com/contest/1236/submission/64706286

---
+++ codeforces/1236/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define uu          first
+#define vv          second
+#define pii         pair<int,int>
+#define INF         1e9
+#define MOD         1000000007
+#define fastRead    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+
+ll bigMod(ll a , ll b) {
+
+    ll p ;
+    if( b == 0) return 1;
+    p = ( bigMod(a , b/2) % MOD );
+    p = ( p * p) % MOD;
+    if( b & 1 ) p = ( p * a) % MOD;
+    return p;
+}
+int main()
+{
+    ll n , m;
+    cin>>n>>m;
+    
+    ll tmp = ( bigMod(2 , m) - 1 + MOD)%MOD;
+    cout<<bigMod(tmp , n)<<'\n';
+    return 0;
+}
