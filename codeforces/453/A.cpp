54be2e8bf8c0250d8dad9c3f6eafd1ac099a0b6c
Mahir Shahriyar <mahirsezan@gmail.com>
2019-11-25 14:44:00 +0600

    Add solution for problem `A - Little Pony and Expected Maximum` Link: https://codeforces.com/contest/453/problem/A Tags: probabilities, *1600 Ref: https://codeforces.com/contest/453/submission/65696926

---
+++ codeforces/453/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define uu          first
+#define vv          second
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define INF         1e9
+#define MOD         1000000007
+#define fastRead    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+
+const int MAX = 1e6 + 6;
+
+
+double power(double a , int b) {
+
+    double p ;
+    if( b == 0) return 1.0;
+    p = ( power(a , b/2));
+    p = ( p * p) ;
+    if( b & 1 ) p = ( p * a) ;
+    return p;
+}
+
+
+int main()
+{
+    double m , n;
+    cin>>m>>n;
+    
+    double sum = 0;
+    for(int i =1;  i< m ; i++ ) {
+        
+        double tmp = (i*1.0)/m;
+        tmp = power(tmp , n);
+        sum += tmp;
+    }
+    
+    sum = m - sum;
+    cout<<setprecision(10)<<fixed<<sum<<'\n';
+    return 0;
+    
+}
