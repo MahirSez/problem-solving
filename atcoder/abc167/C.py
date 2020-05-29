edd3c658febdfa9335e77dfebb84beee2657f77a
Mahir Shahriyar <mahirsezan@gmail.com>
2020-05-29 22:21:00 +0600

    Add solution for problem `C - Skill Up` Link: https://atcoder.jp/contests/abc167/tasks/abc167_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc167/submissions/13693698

---
+++ atcoder/abc167/C.py
@@ -0,0 +1,26 @@
+n, m, x = map(int, input().split())
+inn = [ list( map(int, input().split() ) ) for _ in range(n) ]
+c = [inn[_][0] for _ in range(n) ]
+ara = [ inn[_][1:] for _ in range(n)]
+
+ans = 1e9
+
+for mask in range(2**n):
+	cost = 0
+	cnt = [0 for _ in range(m)]
+	for i in range(n):
+		if ( (1 << i) & mask ) == 0:
+			continue
+		cost += c[i]
+		for j in range(m):
+			cnt[j] += ara[i][j]
+	if min(cnt) >= x:
+		ans = min(ans, cost)
+
+if ans == 1e9:
+	ans = -1
+
+print(ans)
+
+
+
