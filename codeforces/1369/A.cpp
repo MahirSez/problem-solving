a80428750a132981cf0f40aa978007f3f6da9921
Mahir Shahriyar <mahirsezan@gmail.com>
2020-06-23 20:08:00 +0600

    Add solution for problem `A - FashionabLee` Link: https://codeforces.com/contest/1369/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1369/submission/84754655

---
+++ codeforces/1369/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define uu          first
+#define vv          second
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define INF         1e18
+#define fastRead    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+
+const ll MOD = 1e9 + 7;
+const int MAX = 1e6+ 6;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        if(n%4) cout<<"NO\n";
+        else cout<<"YES\n";
+    }
+    return 0;
+}
