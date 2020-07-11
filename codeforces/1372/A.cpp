c026d84027b5b52e0eb831c37579c21dd039b35b
Mahir Shahriyar <mahirsezan@gmail.com>
2020-07-11 21:06:00 +0600

    Add solution for problem `A - Omkar and Completion` Link: https://codeforces.com/contest/1372/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1372/submission/86514379

---
+++ codeforces/1372/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define uu          first
+#define vv          second
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define fastio    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+const int INF = 1e9;
+const ll MOD = 1e9 + 7;
+
+const int N = 1e6 + 6;
+
+int main()
+{
+    fastio;
+	int t;
+	cin>>t;
+	while(t--) {
+		int n;
+		cin>>n;
+		for(int i =1; i <=n ; i++ ) cout<<1<<" ";
+		cout<<'\n';
+	}
+    return 0;
+}
