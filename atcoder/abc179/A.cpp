55521e65ef95f9e8c1a64f58381785f124f97933
Mahir Shahriyar <mahirsezan@gmail.com>
2020-09-19 18:01:00 +0600

    Add solution for problem `A - Plural Form` Link: https://atcoder.jp/contests/abc179/tasks/abc179_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc179/submissions/16847007

---
+++ atcoder/abc179/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define uu          first
+#define vv          second
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define tp          tuple<int,int,int>
+#define fastio      ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+const int INF = 1e9;
+const ll MOD = 1e9 + 7;
+
+const int N = 1e6 + 6;
+
+int main()  {
+    fastio;
+
+    
+    string s;
+    cin>>s;
+    if(s.back() == 's') cout<<s<<"es\n";
+    else cout<<s<<"s\n";
+    return 0;
+}
