ee78185cb8e3ea2cc3c91e068b95d7155bc95d36
Mahir Shahriyar <mahirsezan@gmail.com>
2020-10-07 11:17:00 +0600

    Add solution for problem `B - Integer Preference` Link: https://atcoder.jp/contests/abl/tasks/abl_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abl/submissions/17231883

---
+++ atcoder/abl/B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define uu          first
+#define vv          second
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define tp          tuple<int,int,int>
+#define fastio      ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+const int INF = 1e9;
+const ll MOD = 1e9 + 7;
+
+const int N = 1e6 + 6;
+
+int main()  {
+    fastio;
+
+    ll a , b , c , d;
+    cin>>a>>b>>c>>d;
+    a = max(a , c);
+    b = min(b , d);
+    if(a <=b) cout<<"Yes\n";
+    else cout<<"No\n";
+    return 0;
+    
+}
