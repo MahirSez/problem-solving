298c61c8987e8624e4662cd6fdb20df35528f558
Mahir Shahriyar <mahirsezan@gmail.com>
2020-10-07 11:15:00 +0600

    Add solution for problem `A - Repeat ACL` Link: https://atcoder.jp/contests/abl/tasks/abl_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abl/submissions/17231864

---
+++ atcoder/abl/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define uu          first
+#define vv          second
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define tp          tuple<int,int,int>
+#define fastio      ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+const int INF = 1e9;
+const ll MOD = 1e9 + 7;
+
+const int N = 1e6 + 6;
+
+int main()  {
+    fastio;
+    string s = "ACL";
+    int k;
+    cin>>k;
+    while(k--)  cout<<s;
+    cout<<'\n';
+    return 0;
+    
+    
+}
