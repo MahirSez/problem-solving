e24518ecb1ed7c00442f342f418cf4d0c9bc9778
Mahir Shahriyar <mahirsezan@gmail.com>
2020-12-03 19:05:00 +0600

    Add solution for problem `B - Jumps` Link: https://codeforces.com/contest/1455/problem/B Tags: constructive algorithms, math, *1200 Ref: https://codeforces.com/contest/1455/submission/100272953

---
+++ codeforces/1455/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define uu          first
+#define vv          second
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define tp          tuple<int,int,int>
+#define fastio      ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+const int INF = 1e9;
+const ll MOD = 1e9 + 7;
+
+const int N = 1e6 + 6;
+
+int main()  {
+    fastio;
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        int ans = 0 , now = 0;
+
+        for(int i =1 ; now < n ; i++) {
+            now +=i;
+            ans++;
+        }
+        if( now - n == 1 ) ans++;
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
