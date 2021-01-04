782fcd07f2a8cec69d70eb648f065d520868005f
Mahir Shahriyar <mahirsezan@gmail.com>
2021-01-04 20:48:00 +0600

    Add solution for problem `C - Long Jumps` Link: https://codeforces.com/contest/1472/problem/C Tags: dp, graphs, *1100 Ref: https://codeforces.com/contest/1472/submission/103202850

---
+++ codeforces/1472/C.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll          long long int
+#define uu          first
+#define vv          second
+#define pii         pair<int,int>
+#define pll         pair<ll,ll>
+#define tp          tuple<int,int,int>
+#define fastio      ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+const int INF = 1e9;
+const ll MOD = 1e9 + 7;
+
+const int N = 1e6 + 6;
+
+int main()  {
+    fastio;
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        vector<ll>vec(n) , sum(n);
+        for(int i =0; i <n ; i++ ) cin>>vec[i];
+        ll ans = 0;
+
+        for(int i = n-1 ;  i >=0 ; i--) {
+            
+            sum[i] = vec[i];
+            if(vec[i] + i < n ) sum[i] += sum[i+vec[i]];
+            ans = max(ans , sum[i]);
+        }
+        cout<<ans<<'\n';
+    }
+    return 0;
+
+}
